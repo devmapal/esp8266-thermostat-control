959ce5276f3b38ba4552fa8019cef4c01cb9e9f8
Fabian Schaffert <fabian@schaffert.cc>
2017-01-03 22:11:38 +0100

    Use deep sleep to further reduce power usage

--- thermostat/thermostat.ino
+++ thermostat/thermostat.ino
@@ -2,17 +2,13 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-extern "C" {
-#include "user_interface.h"
-}
-
 #include <ArduinoJson.h>
 
 // Should contain 'wifi_ssid' and 'wifi_password'
 #include "wifi_config.h"
 
 // Pins connected to incremental rotary encoder
-#define OUT0 D0
+#define OUT0 D5
 #define OUT1 D1
 
 #define OLD_TEMP_EEPROM_ADDRESS 0
@@ -39,12 +35,20 @@ void setup_wifi() {
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+
+  // Connect D0 to RST to wake up
+  pinMode(D0, WAKEUP_PULLUP);
 
   setup_wifi();
 
   pinMode(OUT0, OUTPUT);
   pinMode(OUT1, OUTPUT);
+
+  updateTemperatureIfChanged();
+
+  // Go into deep sleep for 60 seconds
+  ESP.deepSleep(60 * 1000000);
 }
 
 void setTemperature(float temp) {
@@ -124,15 +128,4 @@ void updateTemperatureIfChanged() {
   http.end();
 }
 
-void loop() {
-  if(WiFi.status() != WL_CONNECTED) {
-    setup_wifi();
-  }
-
-  updateTemperatureIfChanged();
-
-  // Go into light sleep for 60 seconds
-  wifi_set_sleep_type(LIGHT_SLEEP_T);
-  delay(60000); // 60s
-  wifi_set_sleep_type(NONE_SLEEP_T);
-}
+void loop() {}
